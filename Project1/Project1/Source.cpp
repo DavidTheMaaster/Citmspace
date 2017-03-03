6229b21fb5c97fb3c5f08c2a6400c5b4c44fad19
DavidTheMaaster <davidthegaamer@gmail.com>
2017-03-03 12:00:09 +0100

    Homeworks

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -6,6 +6,8 @@
 #pragma comment (lib, "lib/SDL2.lib")
 #pragma comment (lib, "lib/SDL2main.lib")
 
+int SDL_PollEvent(SDL_Event *event);
+
 int SCREEN_WIDTH = 640;
 int SCREEN_HEIGHT = 480;
 
@@ -14,16 +16,20 @@ int SCREEN_HEIGHT = 480;
 int main(int argc, char* args[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	SDL_Event event;
 
 	SDL_Window* window = NULL;
 	SDL_Surface* screenSurface = NULL;
 	SDL_Surface *rectangle = NULL;
 	SDL_Surface *red = NULL;
 	SDL_Rect square;
+	SDL_Renderer *renderer;
+	renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
 	
 
-	square.x = 0;
-	square.y = 0;
+	square.x = 200;
+	square.y = 200;
 	square.w = 100;
 	square.h = 100;
 
@@ -36,14 +42,34 @@ int main(int argc, char* args[]) {
 	SDL_BlitSurface(red, NULL, screenSurface, NULL);
 	SDL_FreeSurface(red);
 
-	SDL_BlitSurface(rectangle, &square, screenSurface, NULL);
+	SDL_BlitSurface(rectangle, &square, screenSurface, &square);
 	SDL_FreeSurface(rectangle);
 
-
 	SDL_UpdateWindowSurface(window);
+	
 
-	for(;;){ }
+	for (;;) {
+		while (SDL_PollEvent(&event)) {
+			if (event.key.keysym.sym == SDLK_UP) {
+				SDL_DestroyWindow(window);
+				square.x = 110;
+				screenSurface = SDL_GetWindowSurface(window);
+				rectangle = SDL_LoadBMP("colors/blue.bmp");
+				red = SDL_LoadBMP("colors/red.bmp");
 
+				SDL_BlitSurface(red, NULL, screenSurface, NULL);
+				SDL_FreeSurface(red);
+
+				SDL_BlitSurface(rectangle, &square, screenSurface, &square);
+				SDL_FreeSurface(rectangle);
+
+			}
+				
+
+			
+			
+		}
+	}
 
 	SDL_DestroyWindow(window);
 
